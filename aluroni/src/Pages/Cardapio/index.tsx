408025818eb9a5324ea29094ca022215fd66403c
Claudio Anjos <claudio.anjoss@outlook.com>
2023-08-11 19:34:19 -0300

    Ordenador

--- aluroni/src/Pages/Cardapio/index.tsx
+++ aluroni/src/Pages/Cardapio/index.tsx
@@ -1,12 +1,14 @@
-import { useState } from 'react';
+import { useState } from "react";
 import Buscador from "./Buscador";
 import styles from "./Cardapio.module.scss";
 import Logo from "assets/logo.svg";
-import Filtros from './Filtros';
+import Filtros from "./Filtros";
+import Ordenador from "./Ordenador";
 
 export default function Cardapio() {
-    const [ busca, setBusca ] = useState('');
-    const [ filto, setFiltro ] = useState<number | null>(null);
+  const [busca, setBusca] = useState("");
+  const [filto, setFiltro] = useState<number | null>(null);
+  const [ordenador, setOrdenador] = useState('');
 
   return (
     <main>
@@ -21,6 +23,7 @@ export default function Cardapio() {
         <Buscador busca={busca} setBusca={setBusca} />
         <div className={styles.cardapio__filtros}>
           <Filtros filtro={filto} setFiltro={setFiltro} />
+          <Ordenador ordenador={ordenador} setOrdenador={setOrdenador} />
         </div>
       </section>
     </main>
