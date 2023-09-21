c90087a24cccd4f13d273539789dc81c82c5cf07
Claudio Anjos <claudio.anjoss@outlook.com>
2023-09-21 01:49:43 -0300

    Sobre & NotFound
    Desenvolver um tema para a aplicação;
    Aplicar o componente Outlet do react-router-dom;
    Utilizar o hook useNavigate para navegar entre rotas;
    Criar uma tela not found para páginas não encontradas.

--- aluroni/src/pages/Sobre/index.tsx
+++ aluroni/src/pages/Sobre/index.tsx
@@ -1,23 +1,33 @@
-import cardapio from 'data/cardapio.json';
-import styles from './Inicio.module.scss';
+import styles from './Sobre.module.scss';
+import stylesTema from 'styles/Tema.module.scss';
+import casa from 'assets/sobre/casa.png';
+import massa1 from 'assets/sobre/massa1.png';
+import massa2 from 'assets/sobre/massa2.png';
 
-export default function Inicio() {
-  let pratosRecomendados = [...cardapio];
-  pratosRecomendados = pratosRecomendados.sort(() => 0.5 - Math.random()).splice(0,3);
+const imagens = [massa1, massa2];
+
+export default function Sobre() {
   return (
     <section>
-      <h3 className={styles.titulo}>
-        Recomendações da cozinha
-      </h3>
-      <div className={styles.recomendados}>
-        {pratosRecomendados.map(item => (
-          <div key={item.id} className={styles.recomendado}>
-            <div className={styles.recomendado__imagem}>
-              <img src={item.photo} alt={item.title} />
-            </div>
-            <button className={styles.recomendado__botao}>
-              Ver mais
-            </button>
+      <h3 className={stylesTema.titulo}>Sobre</h3>
+      <div className={styles.sobreNos}>
+        <img src={casa} alt="Casa Aluroni" />
+        <div className={styles.sobreNos__texto}>
+          <p>
+            Nós do Aluroni oferecemos a vocês, nossos queridos clientes, a Massa Italiana Caseira mais saborosa e sofisticada de São Paulo! Prezamos pelos ingredientes tradicionais da culinária Italiana, frescos e de excelente qualidade para que sua experiência seja ainda mais intensa!
+          </p>
+          <p>
+            Também possuímos uma cardápio de carnes com muitas opções de acordo com o seu gosto!
+          </p>
+          <p>
+            Para acompanhar as massas italianas, Aluroni possui uma reserva de vinhos especiais, que harmonizam perfeitamente com o seu parto, seja carne ou massa!
+          </p>
+        </div>
+      </div>
+      <div className={styles.imagens}>
+        {imagens.map((imagem, index) => (
+          <div key={index} className={styles.imagens__imagem}>
+            <img src={imagem} alt="Imagem de massa" />
           </div>
         ))}
       </div>
